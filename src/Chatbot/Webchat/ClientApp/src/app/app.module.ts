9bf51e3975317f86a1670c1c3cbb3bba35134f57
OliverCorsino <OliverCorsino@users.noreply.github.com>
2021-10-09 22:47:33 -0400

    Started working with the sign up feature.

--- src/Chatbot/Webchat/ClientApp/src/app/app.module.ts
+++ src/Chatbot/Webchat/ClientApp/src/app/app.module.ts
@@ -1,4 +1,3 @@
-import { DefaultLayoutComponent } from './layouts/default-layout/default-layout.component';
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
@@ -7,15 +6,20 @@ import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { AppRoutes } from './app.routing';
 import { NavMenuComponent } from './components/nav-menu/nav-menu.component';
+import { AuthLayoutComponent } from './layouts/auth-layout/auth-layout.component';
+import { DefaultLayoutComponent } from './layouts/default-layout/default-layout.component';
 import { HomeComponent } from './pages/home/home.component';
+import { SignUpComponent } from './pages/sign-up/sign-up.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     DefaultLayoutComponent,
+    AuthLayoutComponent,
     NavMenuComponent,
     HomeComponent,
+    SignUpComponent
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
--- src/Chatbot/Webchat/ClientApp/src/app/app.routing.ts
+++ src/Chatbot/Webchat/ClientApp/src/app/app.routing.ts
@@ -1,16 +1,27 @@
 import { Routes } from '@angular/router';
-import { HomeComponent } from './pages/home/home.component';
+import { AuthLayoutComponent } from './layouts/auth-layout/auth-layout.component';
 import { DefaultLayoutComponent } from './layouts/default-layout/default-layout.component';
+import { HomeComponent } from './pages/home/home.component';
+import { SignUpComponent } from './pages/sign-up/sign-up.component';
 
 export const AppRoutes: Routes = [
   {
     path: '',
-    pathMatch: 'full',
     component: DefaultLayoutComponent,
     children: [
       {
         path: '',
         component: HomeComponent
+      }
+    ]
+  },
+  {
+    path: '',
+    component: AuthLayoutComponent,
+    children: [
+      {
+        path: 'sign-up',
+        component: SignUpComponent
       }
     ]
   },
